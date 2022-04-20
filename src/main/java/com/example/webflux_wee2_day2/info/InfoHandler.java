8524ec1b45910844a240ff1b39d0ea4434bc5611
Bae <qpyu66@naver.com>
2022-04-20 09:00:41 +0900

    수정

---
+++ src/main/java/com/example/webflux_wee2_day2/info/InfoHandler.java
@@ -0,0 +1,28 @@
+package com.example.webflux_wee2_day2.info;
+
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+
+import java.util.HashMap;
+
+@Component
+public class InfoHandler {
+    private HashMap<Object, Object> result = new HashMap<>();
+    private Mono<HashMap<Object, Object>> mapper = Mono.just(result);
+
+    public Mono<ServerResponse> getByJob(ServerRequest request) {
+        String name = String.valueOf(request.queryParam("name"));
+        System.out.println("name > "+name);
+        result.put("job", name);
+        mapper.subscribe( (arg)->{
+            System.out.println(arg);
+            System.out.println("request > "+request);
+        });
+        return ServerResponse.ok().contentType(MediaType.APPLICATION_JSON).body(BodyInserters.fromProducer(mapper, HashMap.class));
+    }
+
+}
--- src/main/java/com/example/webflux_wee2_day2/info/InfoServiceConfig.java
+++ src/main/java/com/example/webflux_wee2_day2/info/InfoServiceConfig.java
@@ -1,21 +1,30 @@
 package com.example.webflux_wee2_day2.info;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
-import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.server.RequestPredicates;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
 
 import static org.springframework.web.reactive.function.BodyInserters.fromFormData;
 
-//@Configuration
-@Service
-public class InfoService {
+@Configuration
+ public class InfoServiceConfig {
 
 //    WebClient client = WebClient.create("http://localhost:8081");
+@Bean
+public RouterFunction<ServerResponse> route(InfoHandler handler) {
+    return RouterFunctions.route(
+            RequestPredicates.GET("/hello").and(RequestPredicates.accept(MediaType.TEXT_PLAIN)), handler::getByJob);
+}
 
     WebClient webClient = WebClient.builder()
-            .baseUrl("http://localhost:8081")
+            .baseUrl("http://localhost:8080")
             .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
             .build();
 
